ba8495a4347e63a99b84eba1f691b9f88728d249
WPJengineer <jkelly20940@iesjoanramis.org>
2026-01-28 13:24:55 +0100

    adjustments to paths in js

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ const btnHome = document.querySelector("#btnHome");
 const btnProducts = document.querySelector("#btnProducts");
 
 btnHome.addEventListener('click', () => {
-    window.location.href = "/index.html";
+    window.location.href = "/Boci/index.html";
 });
 
 btnProducts.addEventListener('click', () => {
-    window.location.href = "/views/products.html";
+    window.location.href = "/Boci/views/products.html";
 });
